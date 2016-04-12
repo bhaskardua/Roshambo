ab391655b1cb3839b89236bdf7d3b5d1b7220484
Bhaskar Dua <bhaskar_dua@yahoo.com>
2016-04-12 17:04:49 +0800

    Initial commit. Created a new Xcode project using the Single View template. Added a swift file to the project, a subclass of UIViewController, so that we have two view controller files. Gave the view controllers descriptive names. Dragged a second view controller into storyboard. Set its class in the identity inspector. Added buttons and labels. Got first button to present the second view controller through code.

--- Roshambo/GameViewController.swift
+++ Roshambo/GameViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func presentViewControllerWithCode(sender: UIButton) {
+        
+        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 
 }
 
