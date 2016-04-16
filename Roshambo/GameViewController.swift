32fc5458c7974640c1fc9e20669023d28b49e1e9
Bhaskar Dua <bhaskar_dua@yahoo.com>
2016-04-17 02:29:46 +0800

    final commit

--- Roshambo/GameViewController.swift
+++ Roshambo/GameViewController.swift
@@ -23,7 +23,8 @@ class GameViewController: UIViewController {
     var userInput: Input!
     var computerInput: Input!
     var result: Results!
-    
+    var resultText: String!
+    var resultImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,12 +39,17 @@ class GameViewController: UIViewController {
     @IBAction func presentViewControllerWithCode(sender: UIButton) {
         
         let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+        controller.resultText = resultText
+        controller.resultImage = resultImage
         self.presentViewController(controller, animated: true, completion: nil)
+        print("present View Controller with Code")
     }
 
     @IBAction func presentViewControllerWithCodeAndSegue(sender: UIButton) {
         
+        print("present View Controller with Code and Segue - START")
         performSegueWithIdentifier("showResult", sender: self)
+        print("present View Controller with Code and Segue - END")
     }
     
     
@@ -73,33 +79,34 @@ class GameViewController: UIViewController {
         case (.Scissors, .Paper), (.Paper, .Scissors):
             result = Results.ScissorsCutsPaper
         }
+        
+        switch result! {
+        case Results.itsATie:
+            resultText = "It's a tie"
+            resultImage = UIImage(named: "itsATie")!
+        case Results.PaperCoversRock:
+            resultText = "Paper Covers Rock"
+            resultImage = UIImage(named: "PaperCoversRock")!
+        case Results.RockCrushesScissors:
+            resultText = "Rock Crushes Scissors"
+            resultImage = UIImage(named: "RockCrushesScissors")!
+        case Results.ScissorsCutsPaper:
+            resultText = "Scissors Cut Paper"
+            resultImage = UIImage(named: "ScissorsCutPaper")!
+        }
+        
+        print("play Game")
+        
     }
     
     
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showResult" {
+        if segue.identifier == "showResult" || segue.identifier == "showResult2" {
             let resultVC = segue.destinationViewController as! ResultViewController
-            let resultText: String
-            let resultImage: UIImage
-            
-            switch result! {
-            case Results.itsATie:
-                resultText = "It's a tie"
-                resultImage = UIImage(named: "itsATie")!
-            case Results.PaperCoversRock:
-                resultText = "Paper Covers Rock"
-                resultImage = UIImage(named: "PaperCoversRock")!
-            case Results.RockCrushesScissors:
-                resultText = " Rock Crushes Scissors"
-                resultImage = UIImage(named: "RockCrushesScissors")!
-            case Results.ScissorsCutsPaper:
-                resultText = "Scissors Cuts Paper"
-                resultImage = UIImage(named: "ScissorsCutsPaper")!
-            }
-
             resultVC.resultText = resultText
             resultVC.resultImage = resultImage
+            print("prepare For Segue")
         }
     }
     
